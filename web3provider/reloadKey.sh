2b0c85d39f4d360fef14219917d960266d6464f5
lyfsn <dev.wangyu@proton.me>
2023-12-25 17:14:36 +0800

    add upcheck

--- web3provider/reloadKey.sh
+++ web3provider/reloadKey.sh
@@ -3,3 +3,5 @@
 
 
 curl -X POST http://localhost:49000/reload
+curl -X POST http://localhost:49001/reload
+curl -X POST http://localhost:49002/reload
---
+++ web3provider/upcheck.sh
@@ -0,0 +1,3 @@
+curl -X GET http://localhost:49000/upcheck
+curl -X GET http://localhost:49001/upcheck
+curl -X GET http://localhost:49002/upcheck
