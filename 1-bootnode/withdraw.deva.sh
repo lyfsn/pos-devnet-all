a8e9c9e30983a9648c6863d7df934e5b747cb475
lyfsn <dev.wangyu@proton.me>
2023-12-27 16:16:25 +0800

    up

---
+++ 1-bootnode/withdraw.deva.sh
@@ -0,0 +1,23 @@
+
+
+
+
+docker run \
+  --rm \
+  -it \
+  -v $(pwd)/consensus-data:/consensus-data \
+  -v $(pwd)/el-cl-genesis-data:/el-cl-genesis-data \
+  -v $(pwd)/keystore-m_12381_3600_0_0_0-1703236543.json:/keystore-m_12381_3600_0_0_0-1703236543.json \
+  sigp/lighthouse:latest \
+  lighthouse \
+  --datadir=/consensus-data \
+  --testnet-dir=/el-cl-genesis-data/custom_config_data \
+  account validator exit \
+  --keystore /keystore-m_12381_3600_0_0_0-1703236543.json \
+  --beacon-node http://88.99.94.109:5052
+
+
+
+
+
+
