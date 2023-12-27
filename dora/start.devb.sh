8d8e96cf342ff48e56c66e0204b9d0af8e539fb5
lyfsn <dev.wangyu@proton.me>
2023-12-27 15:06:56 +0800

    add dora deva

---
+++ dora/start.devb.sh
@@ -0,0 +1,12 @@
+
+docker run \
+  -d \
+  --restart unless-stopped \
+  --name=dora-devnet \
+  -v $(pwd):/config \
+  -v $(pwd)/el-cl-genesis-data:/el-cl-genesis-data \
+  -p 9777:8080 \
+  -it pk910/dora-the-explorer:latest \
+  -config=/config/devb.yaml
+
+
