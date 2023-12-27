e51160e7224f34a74ef48760b437993998d4675a
lyfsn <dev.wangyu@proton.me>
2023-12-27 16:15:22 +0800

    up

--- 1-bootnode/clear.sh
+++ 1-bootnode/clear.sh
@@ -1,8 +1,8 @@
-sudo rm -rvf execution-data
-sudo rm -rvf consensus-data
+rm -rvf execution-data
+rm -rvf consensus-data
 
-sudo rm -rvf validator_keys/logs
-sudo rm -rvf validator_keys/slashing_protection.sqlite
-sudo rm -rvf validator_keys/slashing_protection.sqlite-journal
-sudo rm -rvf validator_keys/.secp-sk
-sudo rm -rvf validator_keys/api-token.txt
+rm -rvf validator_keys/logs
+rm -rvf validator_keys/slashing_protection.sqlite
+rm -rvf validator_keys/slashing_protection.sqlite-journal
+rm -rvf validator_keys/.secp-sk
+rm -rvf validator_keys/api-token.txt
