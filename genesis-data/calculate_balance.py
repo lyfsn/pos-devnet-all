eeae691b5744d83b04c5d44a1e5073d4c03b66cc
lyfsn <dev.wangyu@proton.me>
2023-12-28 20:21:38 +0800

    udpate calc

--- genesis-data/calculate_balance.py
+++ genesis-data/calculate_balance.py
@@ -1,19 +1,26 @@
 import json
 import sys
 
+def wei_to_eth(wei_balance):
+    return wei_balance / 1e18
+
 def calculate_total_balance(file_path):
     try:
         with open(file_path, 'r') as file:
             genesis_data = json.load(file)
 
-        total_balance = 0
+        total_balance_wei = 0
 
         if 'alloc' in genesis_data:
             for account, details in genesis_data['alloc'].items():
                 if 'balance' in details:
-                    total_balance += int(details['balance'], 16) 
+                    balance_wei = int(details['balance'], 16)  
+                    if balance_wei > 0:
+                        print(f"Account: {account}, Balance: {details['balance']} (wei), {wei_to_eth(balance_wei)} ETH")
+                    total_balance_wei += balance_wei
 
-        return total_balance
+        total_balance_eth = wei_to_eth(total_balance_wei)
+        return total_balance_eth
 
     except Exception as e:
         print(f"Error reading file: {e}")
@@ -25,9 +32,9 @@ if __name__ == "__main__":
         sys.exit(1)
 
     file_path = sys.argv[1]
-    total = calculate_total_balance(file_path)
+    total_eth = calculate_total_balance(file_path)
 
-    if total is not None:
-        print(f"Total Balance: {total}")
+    if total_eth is not None:
+        print(f"Total Balance: {total_eth} ETH")
     else:
         print("Failed to calculate total balance.")
