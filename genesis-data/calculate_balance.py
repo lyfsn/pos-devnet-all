fdbfb71e616ff9b847a50e43395dc21a1cf33bc3
lyfsn <dev.wangyu@proton.me>
2023-12-28 20:24:18 +0800

    fix

--- genesis-data/calculate_balance.py
+++ genesis-data/calculate_balance.py
@@ -2,21 +2,25 @@ import json
 import sys
 
 def wei_to_eth(wei_balance):
-    return wei_balance / 1e18
+    # Convert wei to ETH
+    return wei_balance / 10**18
 
 def calculate_total_balance(file_path):
     try:
+        # Load the JSON file
         with open(file_path, 'r') as file:
             genesis_data = json.load(file)
 
+        # Initialize total balance
         total_balance_wei = 0
 
+        # Iterate and sum the balances of all accounts
         if 'alloc' in genesis_data:
             for account, details in genesis_data['alloc'].items():
                 if 'balance' in details:
-                    balance_wei = int(details['balance'], 16)  
+                    balance_wei = int(details['balance'], 16)  # Convert hex to integer
                     if balance_wei > 0:
-                        print(f"Account: {account}, Balance: {details['balance']} (wei), {wei_to_eth(balance_wei)} ETH")
+                        print(f"Account: {account}, Balance: {details['balance']} (wei), {wei_to_eth(balance_wei):.18f} ETH")
                     total_balance_wei += balance_wei
 
         total_balance_eth = wei_to_eth(total_balance_wei)
@@ -35,6 +39,6 @@ if __name__ == "__main__":
     total_eth = calculate_total_balance(file_path)
 
     if total_eth is not None:
-        print(f"Total Balance: {total_eth} ETH")
+        print(f"Total Balance: {total_eth:.18f} ETH")
     else:
         print("Failed to calculate total balance.")
