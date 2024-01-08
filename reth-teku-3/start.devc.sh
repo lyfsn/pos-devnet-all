64ddf4a19ffa77edd831c14efad368ceb6d9c449
lyfsn <dev.wangyu@proton.me>
2024-01-08 20:30:40 +0800

    up

---
+++ reth-teku-3/start.devc.sh
@@ -0,0 +1,18 @@
+
+
+
+export IP_ADDRESS=168.119.5.82
+
+
+export EL_BOOTNODES="enode://9bc6ebd67307d55efbb7745772d4499381b272d5031995c57437ff3be300130cb319bc0185c63da0f34439cf53a12320d6f56ea635dca9ee0d673a264f0d25fe@88.99.94.109:30303"
+
+
+export CL_TRUSTPEERS="16Uiu2HAmNA3a2oSXUTBrZ6oFRnFRwFrCEML6iv8jtFPomZhr4n1T"
+export CL_BOOTNODES="enr:-Ma4QJ5k2dp2t2DaNuO1AKz5cxCFfenfhnGeuRSaOAZdijtzP_MBr7PahajRbnCrgR1M6T-dBCwFUSWqgEX2lHCG5s-CARSHYXR0bmV0c4j__________4RldGgykG6JYLVAAAAB__________-CaWSCdjSCaXCEWGNebYRxdWljgiMpiXNlY3AyNTZrMaEDjSqqk5siVMhoIjlYhFSU_-a_-TT7cO6zuFReMZli7vaIc3luY25ldHMPg3RjcIIjKIN1ZHCCIyg"
+export CL_CHECKPOINT=http://88.99.94.109:5052/
+
+
+docker compose -f compose.yaml up -d
+
+
+
